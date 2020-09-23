5b12a693e1deed93ae7d68c7aa15c4b525768e91
sharathkumar106 <sharathkr606@gmail.com>
2020-09-23 22:52:59 +0530

    Completed backend logics

--- src/users/pages/Authenticate.js
+++ src/users/pages/Authenticate.js
@@ -2,6 +2,9 @@ import React, { useContext, useState } from 'react';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import Card from '../../shared/components/UIElements/Card';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+
 import { useForm } from '../../shared/hooks/form-hook';
 import { VALIDATOR_EMAIL, VALIDATOR_MAXLENGTH, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/Utils/Validator';
 
@@ -13,6 +16,8 @@ const Authenticate = props => {
     const auth = useContext(AuthContext);
 
     const [isLoginMode, setIsLoginMode] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
 
     const [formState, inputHandler, setFormData] = useForm(
         {
@@ -28,11 +33,11 @@ const Authenticate = props => {
     );
 
     const switchModeHandler = () => {
-        if(!isLoginMode) {
+        if (!isLoginMode) {
             setFormData({
                 ...formState.inputs,
-                name:undefined
-            },formState.inputs.email.isValid && formState.inputs.password.isValid);
+                name: undefined
+            }, formState.inputs.email.isValid && formState.inputs.password.isValid);
         }
         else {
             setFormData({
@@ -48,14 +53,46 @@ const Authenticate = props => {
         console.log(isLoginMode)
     }
 
-    const onAuthenticatonHandler = event => {
+    const onAuthenticatonHandler = async event => {
         event.preventDefault();
-        auth.login();
-        console.log("Authentication Success!");
+
+        if (isLoginMode) {
+
+        } else {
+            try {
+                setIsLoading(true);
+                const response = await fetch('http://localhost:5000/api/users/signup', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        name: formState.inputs.name.value,
+                        email: formState.inputs.email.value,
+                        password: formState.inputs.password.value
+                    })
+                });
+
+                const responseData = await response.json();
+
+                if(!response.ok){
+                    throw new Error(responseData.message);
+                }
+
+                console.log(responseData);
+                setIsLoading(false);
+                auth.login();
+            } catch (error) {
+                console.log(error);
+                setIsLoading(false);
+                setError(error.message || 'Something went wrong, please try again.');
+            }
+        }
     }
 
     return (
         <Card className="authentication card-white">
+            {isLoading && <LoadingSpinner asOverlay />}
             <h2 className="authentication__header">Login Required!</h2>
             <hr />
 
